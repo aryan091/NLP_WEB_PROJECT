31865155e85e89ca2bcb8e6dd3a6d96888d7a865
ARYAN DAFTARI <59964730+aryan091@users.noreply.github.com>
2021-06-01 23:03:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,22 +9,6 @@ import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 
-def text_process(mess):
-    """
-    Takes in a string of text, then performs the following:
-    1. Remove all punctuation
-    2. Remove all stopwords
-    3. Returns a list of the cleaned text
-    """
-    # Check characters to see if they are in punctuation
-    nopunc = [char for char in mess if char not in string.punctuation]
-
-    # Join the characters again to form the string.
-    nopunc = ''.join(nopunc)
-
-    # Now just remove any stopwords
-    return [word for word in nopunc.split() if word.lower() not in stopwords.words('english')]
-
 
 # model load spam classification
 spam_model = joblib.load('models/spammodeljune1.pkl')
