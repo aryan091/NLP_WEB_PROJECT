f5b6fabafbec060f1fe0de93da5ec8ef42abecea
ARYAN DAFTARI <59964730+aryan091@users.noreply.github.com>
2021-06-02 01:13:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from heapq import nlargest
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize, sent_tokenize
+import nlpcloud
 
 # model load spam classification
 spam_model = joblib.load('models/spammodeljune1.pkl')
@@ -96,50 +97,9 @@ def summarize_nlp():
 def sum_route():
     if request.method == 'POST':
         text = request.form['message']
-        stopWords = set(stopwords.words("english"))
-        words = word_tokenize(text)
-
-        # Creating a frequency table to keep the
-        # score of each word
-
-        freqTable = dict()
-        for word in words:
-            word = word.lower()
-            if word in stopWords:
-                continue
-            if word in freqTable:
-                freqTable[word] += 1
-            else:
-                freqTable[word] = 1
-
-        # Creating a dictionary to keep the score
-        # of each sentence
-        sentences = sent_tokenize(text)
-        sentenceValue = dict()
-
-        for sentence in sentences:
-            for word, freq in freqTable.items():
-                if word in sentence.lower():
-                    if sentence in sentenceValue:
-                        sentenceValue[sentence] += freq
-                    else:
-                        sentenceValue[sentence] = freq
-
-        sumValues = 0
-        for sentence in sentenceValue:
-            sumValues += sentenceValue[sentence]
-
-        # Average value of a sentence from the original text
-
-        average = int(sumValues / len(sentenceValue))
-
-        # Storing sentences into our summary.
-        summary = ''
-        for sentence in sentences:
-            if (sentence in sentenceValue) and (sentenceValue[sentence] > (1.2 * average)):
-                summary += " " + sentence
-
-
+        client = nlpcloud.Client("bart-large-cnn", "299877c9eee221a6bbbe3f211e4533df1ae3127a")
+        summary_text = client.summarization(text)
+        summary = summary_text['summary_text']
         return render_template('summarize.html', prediction=summary)
 
 
